6bdb6ba9d373be4ce9eebcf7919b4f6af578e494
Jugalcody <jugalnerist@gmail.com>
2022-01-15 21:21:37 +0530

    Armstrong number

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
-a=int(input("Enter a num : "))
-s=0
-a=str(a)
-for i in a:
-  s=s+(int(i)*int(i)*int(i))
-if int(a)==s:
-      print(f"{a} is an armstrong number ")
-else:
-      print(f"{a} is not an armstrong number")
+def isArmstrong(a): # To check the number is armstrong or not
+   s=0
+   a=str(a)
+   for i in a:
+       s=s+(int(i)*int(i)*int(i))
+   if int(a)==s:
+            return True
+   else:
+            return False
+
 
